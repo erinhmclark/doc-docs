cdfe1d28376ec25a88644e98e599bbaba5c8d543
erinhmclark <erinhannahmary.clark@gmail.com>
2025-01-15 20:32:54 +0000

    Tidy it up

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 from importlib.metadata import metadata
 
 package_metadata = metadata("doc-docs")
@@ -16,15 +13,56 @@ release = package_metadata["version"]
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "autoapi.extension",            # Generate API documentation from docstrings
+    "myst_parser",                  # Markdown support
+    'sphinxcontrib.mermaid',        # Mermaid diagrams
+    "sphinx.ext.autodoc",           #
+    "sphinx.ext.viewcode",          # Source code links
+    "sphinx.ext.napoleon",          # Google-style and NumPy-style docstrings
+    'sphinx.ext.autosummary',       # Summarize module/class/function docs
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 
+# -- AutoAPI Configuration ---------------------------------------------------
+autoapi_type = 'python'
+autoapi_dirs = ["../../src"]
+autodoc_typehints = "signature"     # Include type hints in the signature
+autoapi_ignore = []                 # Ignore specific modules
+autoapi_keep_files = False          # Option to retain intermediate JSON files for debugging
+autoapi_add_toctree_entry = True    # Include API docs in the TOC
+autoapi_template_dir = None         # Use default templates
+autoapi_options = [
+    "members",
+    "undoc-members",
+    "show-inheritance",
+    "show-module-summary",
+    "imported-members",
+]
+
+
+# -- Markdown Support --------------------------------------------------------
+myst_enable_extensions = [
+    "colon_fence",          # ::: fences
+    "deflist",              # Definition lists
+    "html_admonition",      # HTML-style admonitions
+    "html_image",           # Inline HTML images
+    "replacements",         # Substitutions like (C)
+    "smartquotes",          # Smart quotes
+    "linkify",              # Auto-detect links
+    "substitution",         # Text substitutions
+]
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
 # html_static_path = ['_static']
+
